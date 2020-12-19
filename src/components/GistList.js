372fd47c871d39e0e1cd72a6e9750b41b756ba9d
Shahrukh Omar <shahrukhomar@gmail.com>
2020-12-19 20:38:45 +0000

    Using reducer to manage the error and gist state

--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,26 +1,93 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState, useReducer} from 'react';
 import { loadGistsByUser } from '../models/github.js';
 
+const FETCH_SUCCESS = 0;
+const FETCH_ERROR = 1;
+
+/**
+ * State/action reduces for the gist pull. See React's hooks API for more info
+ * 
+ * @method gistListReducer
+ * @param {Object} state 
+ * @param {Object} action
+ * @return {Object} 
+ */
+const gistListReducer = (state, action) => {
+  switch (action.type) {
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+        message: null
+      };
+    case FETCH_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+        data: [],
+        message: action.payload
+      };
+    default:
+      throw new Error();
+  }
+};
+
+/**
+ * GistList react functional component
+ */
 const GistList = () => {
-  const [data, setData] = useState({ gists: [] });
+  const [query, setQuery] = useState('');
+  const [search, setSearch] = useState('');
+
+  const [state, dispatch] = useReducer(gistListReducer, {
+    isLoading: false,
+    isError: false,
+    data: [],
+    message: null
+  });
 
   useEffect(async () => {
-    const gists = await loadGistsByUser('shak');
+    try {
+      if (typeof search === 'string' && search.length > 0) {
+        const response = await loadGistsByUser(search);
 
-    setData({ gists });
-  }, []);
+        if (Array.isArray(response)) {
+          dispatch({ type: FETCH_SUCCESS, payload: response });
+        }
+      }
+    } catch (error) {
+      dispatch({ type: FETCH_ERROR, payload: error.message });
+    }
+  }, [search]); // bind to updates on search term
 
   return (
-    <div className="column">
+    <aside className="column">
+      <form
+        onSubmit={event => { 
+          setSearch(query); 
+          event.preventDefault(); // prevent GET request from triggering on submit
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          placeholder="Github username"
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {state.message}
       <ul>
-        {data.gists.map(gist => (
+        {state.data.map(gist => (
           <li key={gist.id}>
             {gist.description}
           </li>
         ))}
       </ul>
-    </div>
+    </aside>
   );
 }
 
--- src/models/github.js
+++ src/models/github.js
@@ -1,3 +1,5 @@
+const GENERIC_ERROR_MESSAGE = 'There was an error processing the request.';
+
 /**
  * Loads all gists for the given username. Github API has size limits but the resolution to those
  * beyond scope of this sample application and would be happy to discuss in detail later.
@@ -8,6 +10,16 @@
  */
 export const loadGistsByUser = async (username) => {
   const response = await fetch(`https://api.github.com/users/${username}/gists`);
+  if (response.ok) {
+    return await response.json();
+  }
 
-  return await response.json();
+  // this can be further expanded to capture exact error from the error object
+  // and display to user OR capture to server syslog service and generate a
+  // service alert email etc.
+  if (typeof response === 'object' && ('statusText' in response)) {
+    throw new Error(response.statusText);
+  }
+
+  throw new Error(GENERIC_ERROR_MESSAGE);
 }
