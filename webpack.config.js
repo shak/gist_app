0be7b7efbbd9330913ab321f58f1497131c73632
Shahrukh Omar <shahrukhomar@gmail.com>
2020-12-19 16:47:14 +0000

    Including polyfill for babel

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       }
     ]
   },
-  entry: "./src/index.js",
+  entry: ["babel-polyfill", "./src/index.js"],
   output: {
     filename: "app.js",
     path: path.resolve(__dirname, "dist")
