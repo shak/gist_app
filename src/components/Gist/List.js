02b4ab08a4e379069ddde26b2e1ef086d3788531
Shahrukh Omar <shahrukhomar@gmail.com>
2020-12-20 12:41:01 +0000

    Added support for pagination in the list component

--- src/components/Gist/List.js
+++ src/components/Gist/List.js
@@ -3,7 +3,10 @@ import React, {
   useState,
   useReducer } from 'react';
 
-import { isArray } from 'lodash';
+import {
+  isArray,
+  add,
+ subtract } from 'lodash';
 
 import GithubAPIStatefulReducer, {
   FETCH_SUCCESS,
@@ -18,7 +21,8 @@ import GistRow from './Row.js';
  */
 const GistList = () => {
   const [query, setQuery] = useState('');
-  const [search, setSearch] = useState('rdeavila');
+  const [search, setSearch] = useState('');
+  const [gistPage, setGistPage] = useState(1);
 
   const [state, dispatch] = useReducer(GithubAPIStatefulReducer, {
     isLoading: false,
@@ -34,7 +38,7 @@ const GistList = () => {
         dispatch({ type: FETCH_INIT });
 
         // attempt fetch from the API
-        const response = await loadGistsByUser(search);
+        const response = await loadGistsByUser(search, gistPage);
 
         if (isArray(response)) {
           dispatch({ type: FETCH_SUCCESS, payload: response });
@@ -43,13 +47,15 @@ const GistList = () => {
     } catch (error) {
       dispatch({ type: FETCH_ERROR, payload: error.message });
     }
-  }, [search]); // bind to updates on search term
+  }, [search, gistPage]); // bind to updates on search term and pagination
+
 
   return (
     <section>
       <form
         onSubmit={event => {
           setSearch(query);
+          setGistPage(1); // in case user changes the search term on a page greater than 1
           event.preventDefault(); // prevent GET request from triggering on submit
         }}
       >
@@ -62,9 +68,24 @@ const GistList = () => {
         />
         <button className="search_submit" type="submit">Show Gists</button>
       </form>
+      {/* The following can be moved to a reusable component very easily */}
+      <nav className="paginator">
+        <button
+          disabled={(gistPage <= 1)}
+          onClick={() => setGistPage(subtract(gistPage, 1))}>
+            Previous Page
+        </button>
+        <button
+          disabled={(state.data.length === 0)}
+          onClick={() => setGistPage(add(gistPage, 1))}>
+            Next Page
+        </button>
+      </nav>
+      {/* The following can definitely be styled better and/or moved to reusable components */}
       {state.isError ? <p className="error">{state.message}</p> : ''}
       {state.isLoading ? <p className="loading">Loading</p> : ''}
       <main role="main">
+        {state.data.length === 0 && state.isLoading === false && gistPage > 1 ? <p>There are no gists to show</p>: ''}
         {state.data.map(gist => (
           <GistRow key={gist.id} gist={gist} />
         ))}
