ee42e676674eb6bc630047f6e6a5b0b2ed67dabc
Shahrukh Omar <shahrukhomar@gmail.com>
2020-12-20 09:37:42 +0000

    Updated the gist model to add the forks fetch method

--- src/models/github.js
+++ src/models/github.js
@@ -1,19 +1,21 @@
-import { get } from 'lodash';
+import { get, isEmpty } from 'lodash';
 
+const GITHUB_API = 'https://api.github.com';
 const GENERIC_ERROR_MESSAGE = 'There was an error processing the request.';
 
 /**
  * Loads all gists for the given username. Github API has size limits but the resolution to those
  * beyond scope of this sample application and would be happy to discuss in detail later.
- * 
+ *
  * @method loadGistsByUser
- * @param {String} username 
+ * @param {String} username
+ * @param {int} [limit=30]
  * @returns Promise will resolve response JSON
  */
-export const loadGistsByUser = async (username) => {
+export const loadGistsByUser = async (username, limit = 30) => {
   // fetching max gists per page, I can easily implement pagination but keeping this to-the-point
   // for this exercise. Happy to discuss this face to face futher.
-  const response = await fetch(`https://api.github.com/users/${username}/gists?per_page=100`);
+  const response = await fetch(`${GITHUB_API}/users/${username}/gists?per_page=${limit}`);
   if (response.ok) {
     return await response.json();
   }
@@ -23,3 +25,24 @@ export const loadGistsByUser = async (username) => {
   // service alert email etc.
   throw new Error(get(response, 'statusText', GENERIC_ERROR_MESSAGE));
 }
+
+/**
+ * Loads and returns fork resources from the given URL resource
+ *
+ * @method loadForksFromResourceURL
+ * @param {String} resourceURL
+ * @param {int} [limit=100]
+ * @returns Promise will resolve response JSON
+ */
+export const loadForksFromResourceURL = async (resourceURL, limit = 100) => {
+  if (isEmpty(resourceURL) === false) {
+    const response = await fetch(`${resourceURL}?per_page=${limit}`);
+    if (response.ok) {
+      return await response.json();
+    }
+
+    throw new Error(get(response, 'statusText', GENERIC_ERROR_MESSAGE));
+  }
+
+  throw new Error(GENERIC_ERROR_MESSAGE);
+};
